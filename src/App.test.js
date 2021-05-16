5c918c69645f6de4643a3fc194d5930fd169b049
Abcrusne <rusne.pet@gmail.com>
2021-05-16 16:53:09 +0300

    changed test, not finished

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './components/App';
 
-test('renders learn react link', () => {
+test('renders Weather App text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText('Weather App');
   expect(linkElement).toBeInTheDocument();
 });
